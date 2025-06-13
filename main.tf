b9b4b68ba9051b0f688f9be5fc81d5b754565950
rohith200 <42884535+rohith200@users.noreply.github.com>
2025-06-13 18:04:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   ]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("../id_rsa.pub")
+    public_key = file("${path.module}/id_rsa.pub")
   }
 
   os_disk {
