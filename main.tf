54c7fb7f51029f5a47589f1edc7d40b73263af75
Rohith Kandikatla <kandikatlarohith5189@gmail.com>
2025-06-13 17:41:28 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   ]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("C:/Users/Rohith Kandikatla/.ssh/id_rsa.pub")
   }
 
   os_disk {
