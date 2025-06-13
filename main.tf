6684e95027c82fe7ed748b50042d80d1f2d759c5
rohith200 <42884535+rohith200@users.noreply.github.com>
2025-06-13 15:26:01 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,139 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+#-------------------
+# Linux VM
+#-------------------
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-main"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-main"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "linux_nic" {
+  name                = "linux-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                = "linuxvm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.linux_nic.id,
+  ]
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18_04-lts"
+    version   = "latest"
+  }
+
+  computer_name = "linuxvm"
+}
+
+# Linux VM Data Disk
+resource "azurerm_managed_disk" "linux_data_disk" {
+  name                 = "linuxdatadisk"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "linux_attach" {
+  managed_disk_id    = azurerm_managed_disk.linux_data_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.linux_vm.id
+  lun                = 0
+  caching            = "ReadWrite"
+}
+
+#-------------------
+# Windows VM
+#-------------------
+
+resource "azurerm_network_interface" "windows_nic" {
+  name                = "windows-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  name                = "windowsvm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.windows_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  computer_name = "windowsvm"
+}
+
+# Windows VM Data Disk
+resource "azurerm_managed_disk" "windows_data_disk" {
+  name                 = "windowsdatadisk"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "windows_attach" {
+  managed_disk_id    = azurerm_managed_disk.windows_data_disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.windows_vm.id
+  lun                = 0
+  caching            = "ReadWrite"
+}
